46f3cd25f2c6338fc9553e57b1f2de1df42684a4
Arjun Kundur <arjunkundur@gmail.com>
2025-03-25 23:50:12 +0530

    sdfsdfsd

--- ml-pipeline/scripts/run_training.py
+++ ml-pipeline/scripts/run_training.py
@@ -19,7 +19,7 @@ def main():
     training_params = {
         "TrainingJobName": training_job_name,
         "AlgorithmSpecification": {
-            "TrainingImage": "763104351884.dkr.ecr.ap-south-1.amazonaws.com/xgboost:latest",
+            "TrainingImage": "975050337104.dkr.ecr.ap-south-1.amazonaws.com/xgboost:latest",
             "TrainingInputMode": "File",
         },
         "RoleArn": args.role,
