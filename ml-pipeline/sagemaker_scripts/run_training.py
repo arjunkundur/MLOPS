b429594f6b8b14b2f4577f560144fc958c88a187
Arjun Kundur <arjunkundur@gmail.com>
2025-03-26 11:48:22 +0530

    Updated

--- ml-pipeline/sagemaker_scripts/run_training.py
+++ ml-pipeline/sagemaker_scripts/run_training.py
@@ -1,63 +1,52 @@
-import argparse
 import boto3
 import time
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--project', type=str, required=True, help='SageMaker project name')
-    parser.add_argument('--region', type=str, required=True, help='AWS region')
-    parser.add_argument('--role', type=str, required=True, help='SageMaker execution role ARN')
-    args = parser.parse_args()
+sm_client = boto3.client('sagemaker', region_name='ap-south-1')
 
-    # Initialize SageMaker client
-    sm_client = boto3.client('sagemaker', region_name=args.region)
+project = "my-sagemaker-aws-jenkins-project1"
+role = "arn:aws:iam::975050337104:role/service-role/AmazonSageMaker-ExecutionRole-20250311T162664"
+training_job_name = f"{project}-training-{int(time.time())}"
 
-    # Define job name with timestamp to avoid collisions
-    training_job_name = f"{args.project}-training-{int(time.time())}"
-
-    # Define training job parameters with correctly prefixed hyperparameters
-    training_params = {
-        "TrainingJobName": training_job_name,
-        "AlgorithmSpecification": {
-            "TrainingImage": "975050337104.dkr.ecr.ap-south-1.amazonaws.com/xgboost:latest",
-            "TrainingInputMode": "File",
-        },
-        "RoleArn": args.role,
-        "InputDataConfig": [
-            {
-                "ChannelName": "train",
-                "DataSource": {
-                    "S3DataSource": {
-                        "S3Uri": f"s3://{args.project}/train",
-                        "S3DataType": "S3Prefix",
-                        "S3DataDistributionType": "FullyReplicated",
-                    }
-                },
-                "ContentType": "text/csv",
-            }
-        ],
-        "OutputDataConfig": {
-            "S3OutputPath": f"s3://{args.project}/output",
-        },
-        "ResourceConfig": {
-            "InstanceType": "ml.m5.large",
-            "InstanceCount": 1,
-            "VolumeSizeInGB": 10,
-        },
-        "StoppingCondition": {"MaxRuntimeInSeconds": 3600},
-
-        # Corrected hyperparameters with "--" prefix
-        "HyperParameters": {
-            "--train": "/opt/ml/input/data/train/train.csv",
-            "--model-dir": "/opt/ml/model",
+training_params = {
+    "TrainingJobName": training_job_name,
+    "AlgorithmSpecification": {
+        "TrainingImage": "975050337104.dkr.ecr.ap-south-1.amazonaws.com/xgboost:latest",
+        "TrainingInputMode": "File",
+    },
+    "RoleArn": role,
+    "InputDataConfig": [
+        {
+            "ChannelName": "train",
+            "DataSource": {
+                "S3DataSource": {
+                    "S3Uri": f"s3://{project}/train",
+                    "S3DataType": "S3Prefix",
+                    "S3DataDistributionType": "FullyReplicated",
+                }
+            },
+            "ContentType": "text/csv",
         }
+    ],
+    "OutputDataConfig": {
+        "S3OutputPath": f"s3://{project}/output",
+    },
+    "ResourceConfig": {
+        "InstanceType": "ml.m5.large",
+        "InstanceCount": 1,
+        "VolumeSizeInGB": 10,
+    },
+    "StoppingCondition": {"MaxRuntimeInSeconds": 3600},
+
+    # Check how hyperparameters are passed
+    "HyperParameters": {
+        "--train": "/opt/ml/input/data/train/train.csv",
+        "--model-dir": "/opt/ml/model",
     }
+}
 
-    # Start the training job on SageMaker
-    response = sm_client.create_training_job(**training_params)
-    
-    # Print only the job name for Jenkins
-    print(training_job_name)
+# Output what is being passed to SageMaker for review
+import pprint
+pprint.pprint(training_params)
 
-if __name__ == '__main__':
-    main()
+response = sm_client.create_training_job(**training_params)
+print(f"Training Job created: {training_job_name}")
