3b9c9f29280f7ba910628cc0febd39a03dee42c1
Arjun Kundur <arjunkundur@gmail.com>
2025-03-26 12:30:13 +0530

    Updated

--- ml-pipeline/train.py
+++ ml-pipeline/train.py
@@ -1,26 +1,23 @@
-import argparse
 import pandas as pd
 import os
 import xgboost as xgb
 
-def model_training(train_data_path, model_output_path):
-    # Load and preprocess data
-    data = pd.read_csv(train_data_path)
+def model_training():
+    # SageMaker default paths
+    input_path = '/opt/ml/input/data/train/'
+    model_path = '/opt/ml/model/'
+    
+    # Load training data (assuming file is named train.csv)
+    data = pd.read_csv(os.path.join(input_path, 'train.csv'))
     X = data.iloc[:, :-1]
     y = data.iloc[:, -1]
 
-    # Train a basic XGBoost model
+    # Train model
     model = xgb.XGBClassifier()
     model.fit(X, y)
 
-    # Save the trained model
-    model.save_model(os.path.join(model_output_path, "xgboost-model.json"))
+    # Save model
+    model.save_model(os.path.join(model_path, "xgboost-model.json"))
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--train', type=str, required=True, help='Path to training data')
-    parser.add_argument('--model-dir', type=str, required=True, help='Directory to save the trained model')
-    args = parser.parse_args()
-
-    # Start model training
-    model_training(train_data_path=args.train, model_output_path=args.model_dir)
+    model_training()
